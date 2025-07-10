cdfd33fd8cfdeaaad6fac0bcf079a8a10ef3bfb9
rchacons <rchacons97@gmail.com>
2025-07-10 17:09:55 +0200

    Remove hardcoded secrets

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
-// Database configuration
-$db_host = '34.155.93.15';
-$db_name = 'image_catalog';
-$db_user = 'appmod-phpapp-user';
-$db_pass = 'workshop-test-password';
-
+// Database configuration with ENV variables. Set default values as well 
+$db_host = getenv('DB_HOST') ?: 'localhost';
+$db_name = getenv('DB_NAME') ?: 'image_catalog';
+$db_user = getenv('DB_USER') ?: 'appmod-phpapp-user';
+$db_pass = getenv('DB_PASS') ?: 'wrong_password';
+// Note getenv() is PHP 5.3 compatible
 try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
